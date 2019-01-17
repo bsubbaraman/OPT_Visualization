2b63aa25ec40b172c301e7a5fd8d4e600640cf82
bsubbaraman <blair@remap.ucla.edu>
2019-01-17 14:20:10 -0800

    added camera label

---
+++ Assets/LabelAlign.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabelAlign : MonoBehaviour {
+    /// <summary>
+    /// Camera label always needs to appear
+    /// </summary>
+	// Use this for initialization
+
+    public Camera main;
+	void Start () {
+        Transform camLabel = this.transform;
+        Vector3 camR = camLabel.parent.rotation.eulerAngles;
+        camLabel.localRotation = Quaternion.Euler(-camR);
+
+        camLabel.Translate(.1f * Vector3.up);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 v = main.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        transform.LookAt(main.transform.position - v);
+        transform.Rotate(0, 180, 0);
+    }
+}
